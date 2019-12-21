7e526c09abdee0f40dd2ced4975b7af814f9cec3
jainbhupesh533 <jainbhupesh533@gmail.com>
2019-12-21 19:33:05 +0530

    REset Button

--- lib/result.dart
+++ lib/result.dart
@@ -2,18 +2,19 @@ import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
   final int resultScore;
+  final Function resetHandler;
 
-  Result(this.resultScore);
+  Result(this.resultScore, this.resetHandler);
 
-  String get resultPhrase{
+  String get resultPhrase {
     String resultText;
-    if(resultScore <= 8){
+    if (resultScore <= 8) {
       resultText = 'You are awesome and Innocent!';
-    }else if(resultScore <= 12) {
-       resultText = 'You are PRETTY LIKEABLE!';
+    } else if (resultScore <= 12) {
+      resultText = 'You are PRETTY LIKEABLE!';
     } else if (resultScore <= 16) {
-       resultText = 'You are ... Strange?';
-    }else{
+      resultText = 'You are ... Strange?';
+    } else {
       resultText = 'You are so bad!';
     }
     return resultText;
@@ -22,10 +23,19 @@ class Result extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text(
-        resultPhrase,
-        style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
-        textAlign: TextAlign.center,
+      child: Column(
+        children: <Widget>[
+          Text(
+            resultPhrase,
+            style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+          ),
+          FloatingActionButton(
+            onPressed: resetHandler,
+            tooltip: 'Reset',
+            child: Icon(Icons.refresh),
+          ),
+        ],
       ),
     );
   }
