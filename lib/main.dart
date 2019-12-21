8025cc213428e964b94de45b15aa000db43b57c5
jainbhupesh533 <jainbhupesh533@gmail.com>
2019-12-21 11:00:35 +0530

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,86 @@
+import 'package:first_app/result.dart';
+import 'package:flutter/material.dart';
+import './quiz.dart';
+import './result.dart';
+
+/*
+void main(){
+  runApp(MyApp());
+}*/
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  final _questions = const [
+    {
+      'questionText': 'What\'s your favorite color?',
+      'answers': [
+        {'text': 'Black', 'score': 10},
+        {'text': 'Red', 'score': 5},
+        {'text': 'Green', 'score': 3},
+        {'text': 'White', 'score': 1},
+      ],
+    },
+    {
+      'questionText': 'What\'s your favorite animal?',
+      'answers': [
+        {'text': 'Dog', 'score': 3},
+        {'text': 'Peacock', 'score': 2},
+        {'text': 'Lion', 'score': 4},
+        {'text': 'Tiger', 'score': 5},
+      ],
+    },
+    {
+      'questionText': 'What\'s your favorite Subject?',
+      'answers': [
+        {'text': 'English', 'score': 1},
+        {'text': 'History', 'score': 6},
+        {'text': 'Maths', 'score': 9},
+        {'text': 'Science', 'score': 10},
+      ],
+    },
+  ];
+
+  var _questionIndex = 0;
+  var _totalScore = 0;
+
+  void _answerQuestion(int score) {
+    _totalScore += score;
+
+    setState(() {
+      _questionIndex += 1;
+    });
+    print(_questionIndex);
+    if (_questionIndex < _questions.length) {
+      print("We have more questions");
+    } else {
+      print('No more questions! ');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                answerQuestion: _answerQuestion,
+                questionIndex: _questionIndex,
+                questions: _questions,
+              )
+            : Result(_totalScore),
+      ),
+    );
+  }
+}
